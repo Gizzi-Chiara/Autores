1e0db11edd07d577561d9d5aa85646258146ae11
Chiara Gizzi <chiaragizzi1@gmail.com>
2023-09-12 17:40:37 -0600

    Autores_final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import TodosAutores from "./components/TodosAutores";
+import NuevoAutor from "./components/NuevoAutor";
+import ActualizarAutor from "./components/ActualizarAutor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div className="container">
+      <h1>Autores</h1>
+      
+      <Routes>
+        <Route path="/" exact element={<TodosAutores/>}>Nuevo Autor</Route>
+        <Route path="/nuevo"  element={<NuevoAutor/>}>Nuevo Autor</Route>
+        <Route path="/editar/:id" element={<ActualizarAutor/>}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
